96d41cdbe6ad78f3d5dcee576398c12db0adf2ff
JerrymihMercado <116856008+JerrymihMercado@users.noreply.github.com>
2023-05-21 19:35:01 +0800

    about us

---
+++ about_us.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta http-equiv="x-ua-compatible" content="ie=edge" />
+    <title>Office of Student Affairs</title>
+    <link rel="stylesheet" href="./Style/style.css">
+    <link rel="stylesheet" href="./Style/style_about.css">
+     <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"/>
+    <!-- Google Fonts Roboto -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap"/>
+    <link rel="stylesheet" href="css/mdb.min.css" />
+  </head>
+  <body>
+    <!-- Start your project here-->
+    <?php include './Components/header.php'?>
+    
+    <div class="header_section container-fluid text-center">
+        <div class="row">
+            <div class="col-12 text-center pt-5">
+                <img src="img/white-logo.png" alt="" class="banner_logo ">
+                <h2 class="text-center banner_text text-white">OFFICE OF STUDENT AFFAIRS</h2>    
+                <h3 class="text-white"></h3>
+            </div>
+            <div class="col-12 d-flex justify-content-center pt-5">
+              <div class="card overview">
+                <div class="card-body pt-5   ">
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                  <button type="button" class="btn btn-primary">Button</button>
+                </div>
+              </div>
+            </div>
+        </div>
+    </div>
+    
+    <div class="container">
+      <div class="row">
+        <div class="col-12">
+          <div class="card shadow-5">
+            <div class="row no-gutter">
+              <div class="col-md-3">
+                  <img src="img/osa-logo.jpg" alt="" class="card-img ">
+              </div>
+              <div class="col-md-9">
+                <div class="card-body ">
+                  <h5 class="text-success">OSA MISSION</h5>
+                  <div class="borders" >
+                    <p class=" mission-info">OSA shall promote the development of the students’ talents,
+                      potentials and leadership capabilities through its program thrusts that promote
+                      self- awareness, self-growth and development, self- management, cooperative
+                      living and learning, leadership advancement, social responsibility, nationalism
+                      and patriotism and wise use and management of relevant information.</p>
+                    </div>
+                </div>
+              </div>
+            </div>  
+          </div>
+        </div>
+        <div class="col-12 pt-3">
+          <div class="card shadow-5">
+            <div class="row no-gutter">
+              <div class="col-md-9">
+                <div class="card-body ">
+                  <h5 class="text-success">OSA VISION</h5>
+                  <div class="borders" >
+                    <p class="vision-info ps-3">OSA-CLSU as a model center for student personnel services
+                      supportive of the co-curricular and extra-curricular needs of its clients for their
+                      well- rounded growth and development</p>
+                    </div>
+                </div>
+              </div>
+              <div class="col-md-3">
+                  <img src="img/osa-picture.jpg" alt="" class="card-img ">
+              </div>
+            </div>  
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="container pt-5">
+      <div class="row">
+        <div class="osa-tag">
+          <p class="tag-info">OSA ADMINISTRATION</p>
+          <p class="tag-sub ">Meet the Administrators and Staff</p>
+        </div>
+
+      </div>
+
+    </div>
+  
+
+  
+    <?php include './Components/footer.php'?>
+    <!-- End your project here-->
+
+    <!-- MDB -->
+    <script type="text/javascript" src="js/mdb.min.js"></script>
+    <!-- Custom scripts -->
+    <script type="text/javascript"></script>
+  </body>
+</html>
