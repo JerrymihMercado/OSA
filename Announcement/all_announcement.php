1bc19600be66118236d19329b46505dc65f2860d
JerrymihMercado <116856008+JerrymihMercado@users.noreply.github.com>
2023-05-22 10:28:27 +0800

    today 1026.2205

--- Announcement/all_announcement.php
+++ Announcement/all_announcement.php
@@ -31,7 +31,7 @@
       <nav class="navbar main-header-mid navbar-expand-md">
         <ul class="navbar-nav">
           <li class="nav-item ">
-            <a href="index.php" class="link-home text-white">HOME</a>
+            <a href="../index.php" class="link-home text-white">HOME</a>
           </li>
           <li class="nav-item ">
             <a href="#" class="link text-white ps-3">ABOUT US</a>
@@ -58,38 +58,50 @@
       </nav>
     </div>
   </div>
-  <!-- banner -->
-    <div class="bg-image ripple" data-mdb-ripple-color="light">
-        <img src="../img/clsu-1.jpg" class="banner__img" />
-        <a href="#!">
-            <div class="mask" style="background-color: hsla(0, 0%, 0%, 0.5)">
-            <div class="d-flex justify-content-center align-items-center h-100 text-center">
-                <h2 class="text-white mb-0">PUBLICATIONS</h2>
-            </div>
-            </div>
-            <!-- <div class="hover-overlay">
-            <div class="mask" style="background-color: hsla(0, 0%,98%, 0.2)"></div>
-            </div> -->
-        </a>
-    </div> 
 
     <div class="mt-3 p-3">
         <nav aria-label="breadcrumb">
             <nav aria-label="breadcrumb">
             <ol class="breadcrumb">
-                <li class="breadcrumb-item"><a href="../Section/impu.php">Publication</a></li>
+                <li class="breadcrumb-item"><a href="../index.php">Announcement</a></li>
                 <li class="breadcrumb-item active" aria-current="page">View All</li>
             </ol>
             </nav>
         </nav>
     </div>
 
-    <div class="container-fluid mt-4">
+    <div class="p-2">
+        <div class="card mb-3 bg-success text-white" style="max-width: 100%;">
+            <div class="row g-0">
+                <div class="col-md-4">
+                    <img
+                        src="../img/Rectangle 290.png"
+                        alt="Trendy Pants and Shoes"
+                        class="img-fluid rounded-start"
+                    />
+                    </div>
+                    <div class="col-md-8">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">
+                        This is a wider card with supporting text below as a natural lead-in to
+                        additional content. This content is a little bit longer.
+                        </p>
+                        <p class="card-text">
+                        <small class="text-white">Last updated 3 mins ago</small>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+    <div class="container-fluid">
         <div class="row row-cols-1 row-cols-md-3 g-4">
             <div class="col">
-                <a href="../Publications/publication_page_1.php">
+                <a href="../Announcement/announcement_details.php">
                     <div class="card h-100">
-                    <img src="../img/clsu-1.jpg" class="card-img-top" alt="Hollywood Sign on The Hill"/>
+                    <img src="../img/Rectangle 266.png" class="card-img-top" alt="Hollywood Sign on The Hill"/>
                     <div class="card-body">
                         <h5 class="card-title">Card title</h5>
                         <p class="card-text">
@@ -102,13 +114,23 @@
             </div>
             <div class="col">
                 <div class="card h-100">
-                <img src="../img/clsu-1.jpg" class="card-img-top" alt="Palm Springs Road"/>
+                <img src="../img/Rectangle 266.png" class="card-img-top" alt="Palm Springs Road"/>
                 <div class="card-body">
                     <h5 class="card-title">Card title</h5>
                     <p class="card-text">This is a short card.</p>
                 </div>
                 </div>
             </div>
+            <div class="col">
+                <div class="card h-100">
+                <img src="../img/Rectangle 266.png" class="card-img-top" alt="Los Angeles Skyscrapers"/>
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+                </div>
+                </div>
+            </div>
+        
         </div>
     </div>
 
